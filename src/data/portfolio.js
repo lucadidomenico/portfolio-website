17541bb97ebf7ee4826550f39907af1fd6fed5d4
Luca Di Domenico <56132403+lucadidomenico@users.noreply.github.com>
2023-02-11 18:26:47 +0100

    first commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Terminal Website",
+    imgUrl: "/assets/ai-nerf-gun.jpg",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "www.google.com",
+  },
+  {
+    title: "Developer Portfolio",
+    imgUrl: "/assets/dev-portfolio.jpg",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "www.facebook.com",
+  },
+  {
+    title: "AI Nerf Gun",
+    imgUrl: "/assets/terminal-website.jpg",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "www.youtube.com",
+  },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    year: "Today",
+    title: "Youtube Content Creator",
+    duration: "6 years",
+    details: "jsbfibsdibdsfkjfvbdskfbkjdsbf jksdbfkjdsbfkjsdbfkjbsdfkjbsdfkj",
+  },
+  {
+    year: "2021",
+    title: "Developer",
+    duration: "6 years",
+    details: "jsbfibsdibdsfkjfvbdskfbkjdsbfj ksdbfkjdsbfkjsdbfkjbsdfkjbsdfkj",
+  },
+  {
+    year: "2019",
+    title: "CyberSecurity",
+    duration: "6 years",
+    details: "jsbfibsdibdsfkjfvbdskfbkjdsbf jksdbfkjdsbfkjsdbfkjbsdfkjbsdfkj",
+  },
+  {
+    year: "2018",
+    title: "Youtube Content Creator",
+    duration: "4 years",
+    details: "jsbfibsdibdsfkjfvbdskfbkjdsbfj ksdbfkjdsbfkjsdbfkjbsdfkjbsdfkj",
+  },
+  {
+    year: "2016",
+    title: "Youtube Content Creator",
+    duration: "6 years",
+    details: "jsbfibsdibdsfkjfvbdskfbkjdsb fjksdbfkjdsbfkjsdbfkjbsdfkjbsdfkj",
+  },
+];
