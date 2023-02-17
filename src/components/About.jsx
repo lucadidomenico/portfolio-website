03cf6f28e180b66003fe467aa3b8fc0520a4ed51
Luca Di Domenico <56132403+lucadidomenico@users.noreply.github.com>
2023-02-17 17:32:51 +0100

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,12 +18,13 @@ function About() {
             <h1 className="text-3xl dark:text-white mb-1 mt-5 md:mt-1 md:mb-3 font-bold">
               Hey, I'm Luca.
             </h1>
-            I'm a Blockchain Engineer obsessed with technical details. I have 6
-            years of experience as a Software Security Consultant, Software
-            Developer, and Trainer. Working in cybersecurity field has altered
-            my perspective to prioritize technical understanding above all else:
-            in order to effectively hack a computer, it's important to have a
-            thorough comprehension of how it works.
+            I work at the interception between Software Development and Software
+            Security. I have 6 years of experience as a Software Security
+            Consultant, Software Developer, and Trainer. Working in
+            cybersecurity field has altered my perspective to prioritize
+            technical understanding above all else: in order to effectively hack
+            a computer, it's important to have a thorough comprehension of how
+            it works.
             <AboutItem classe="pt-1" name="Age" value="29" />
             <AboutItem name="Gender" value="Man" />
             <AboutItem name="Language" value="English, Italian" />
